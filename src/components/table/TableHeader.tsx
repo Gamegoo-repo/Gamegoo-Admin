ec8e4e9c93bb0636a65adc06fac7f1779a2d0d8e
yyypearl <hor629@naver.com>
2025-03-31 22:33:30 +0900

    feat(#1): 신고 목록 테이블 내 Checkbox 상태 적용

--- src/components/table/TableHeader.tsx
+++ src/components/table/TableHeader.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import { theme } from "../../styles/theme";
+import Checkbox from "../common/Checkbox";
 
 interface TableHeaderProps {
   columns: string[];
+  allSelected: boolean;
+  onSelectAll: () => void;
 }
 
-export const TableHeader: React.FC<TableHeaderProps> = ({ columns }) => {
+export const TableHeader: React.FC<TableHeaderProps> = ({
+  columns,
+  allSelected,
+  onSelectAll,
+}) => {
   return (
     <THead>
       <tr>
+        <th>
+          <Checkbox checked={allSelected} onChange={onSelectAll} />
+        </th>
         {columns.map((column, index) => (
           <th key={index}>{column}</th>
         ))}
@@ -23,6 +33,5 @@ const THead = styled.thead`
   color: ${theme.colors.white};
   background: ${theme.colors.gray700};
   ${theme.fonts.semiBold14};
-
   border-radius: 6px;
 `;
