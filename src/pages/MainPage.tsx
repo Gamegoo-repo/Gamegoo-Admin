577df9a6090ee16a3890139e8d92c610243f0f9e
yyypearl <hor629@naver.com>
2025-03-31 22:34:05 +0900

    design(#1): 루트 페이지 라우트 이동 버튼 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/common/Button";
+
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div>
+      <li>
+        <Button
+          variant="primary"
+          label="신고 유저 목록 이동"
+          width="250px"
+          height="33px"
+          onClick={() => {
+            navigate("/report");
+          }}
+        />
+      </li>
+    </div>
+  );
 };
 
 export default MainPage;
