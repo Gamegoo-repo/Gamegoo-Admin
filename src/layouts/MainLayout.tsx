86f40375f22967a77593bdf64e0191654eb237c4
Sungwook Kim <silenc3@naver.com>
2025-07-17 12:17:51 +0900

    style(#13): prettier format 실행

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,8 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import Header from "../components/common/Header";
-import styled from "styled-components";
 import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+import Header from "../components/common/Header";
 
 const MainLayout = () => {
   const navigate = useNavigate();
